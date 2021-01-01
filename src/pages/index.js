236b7b597e88eb60a89d9f2bdd12b622e7a5366d
Matt Heslington <matt.heslington@icloud.com>
2021-01-01 15:25:53 +0800

    All seems to be working nice

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { motion } from "framer-motion";
+import {Helmet} from "react-helmet";
 import Layout from 'src/components/Layout'
 import red from 'src/static/red.png'
 import blue from 'src/static/blue.png'
@@ -10,29 +12,37 @@ import fast from 'src/static/fast.png'
 // markup
 const IndexPage = () => {
 
+    const defaultHero = {scale: 0.92};
+    const defaultAward = {opacity:0, scale:0.8}
+
     return (
         <Layout>
+            <Helmet>
+                <title>P E T A L .</title>
+            </Helmet>
             <div className="flex flex-col items-center justify-center w-full h-screen mt-20">
-                <h2 className="mb-12 text-5xl font-black leading-tight tracking-tight text-center text-white xl:text-10xl lg:text-8xl md:text-7xl sm:text-6xl">
-                    <span className="gradienta">Cost-effective</span>
-                    <br/>
-                    web development
-                    <br/>
-                    <span className="gradienta">you'll adore</span>
-                </h2>
+                <motion.div initial={defaultHero} animate={{scale: 1}} transition={{ duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] }}>
+                    <h2 className="mb-12 text-5xl font-black leading-tight tracking-tight text-center text-white xl:text-10xl lg:text-8xl md:text-7xl sm:text-6xl">
+                        <span className="gradienta">Cost-effective</span>
+                        <br/>
+                        web development
+                        <br/>
+                        <span className="gradienta">you'll adore</span>
+                    </h2>
+                </motion.div>
                 <div className="flex items-center justify-around w-full mb-16">
-                    <div className="flex flex-col items-center space-y-5">
-                        <img className="w-24" src={tech} alt="technology"/>
-                        <p className="text-sm font-light">We use cutting-edge technology</p>
-                    </div>
-                    <div className="flex flex-col items-center space-y-5">
-                        <img className="w-24" src={custom} alt="custom made"/>
-                        <p className="text-sm font-light">Our sites are lovingly custom-made</p>
-                    </div>
-                    <div className="flex flex-col items-center space-y-5">
-                        <img className="w-24" src={fast} alt="fast"/>
-                        <p className="text-sm font-light">Both our websites & service are super fast</p>
-                    </div>
+                    <motion.div initial={defaultAward} animate={{scale: 1, opacity:1}} transition={{ delay:0.7, duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] }} className="flex flex-col items-center space-y-5 xl:w-48">
+                        <img className="w-20" src={tech} alt="technology"/>
+                        <p className="text-xs font-light text-center">All our sites are bespoke & custom-made to our clients exact requirements</p>
+                    </motion.div>
+                    <motion.div initial={defaultAward} animate={{scale: 1, opacity:1}} transition={{ delay:0.9, duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] }} className="flex flex-col items-center space-y-5 xl:w-48">
+                        <img className="w-20" src={custom} alt="custom made"/>
+                        <p className="text-xs font-light text-center">We use the best, cutting-edge technology to give the user the best possible experience</p>
+                    </motion.div>
+                    <motion.div initial={defaultAward} animate={{scale: 1, opacity:1}} transition={{ delay:1.1, duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] }} className="flex flex-col items-center space-y-5 xl:w-48">
+                        <img className="w-20" src={fast} alt="fast"/>
+                        <p className="text-xs font-light text-center">All our sites are optimised for SEO, performance and accessibility</p>
+                    </motion.div>
                 </div>
             </div>
 
--- src/pages/work-with-petal.js
+++ src/pages/work-with-petal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Helmet} from "react-helmet";
 import Layout from 'src/components/Layout'
 
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
@@ -11,43 +12,69 @@ import StarIcon from 'src/components/Icons'
 const WorkWith = () => {
     return (
         <Layout>
-            <div className="flex justify-center">
-                <VerticalTimeline>
-                    <VerticalTimelineElement className="vertical-timeline-element--work" contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }} date="2011 - present" iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} icon={WorkIcon}>
+
+            <Helmet>
+                <title>Working with P E T A L .</title>
+            </Helmet>
+
+            <div className="flex flex-col items-center justify-center">
+
+                <div className="flex justify-center mt-16">
+                    <h2 className="z-10 my-40 font-bold text-8xl gradienta"><span>Work with Petal</span></h2>
+                </div>
+
+                <VerticalTimeline className={'border border-2 rounded-lg bg-darkBG'}>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        dateClassName="text-red-500 text-3xl font-bold"
+                        date="We listen"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={WorkIcon}>
                         <h3 className="vertical-timeline-element-title">Creative Director</h3>
                         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                         <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
                     </VerticalTimelineElement>
-                    <VerticalTimelineElement className="vertical-timeline-element--work" date="2010 - 2011" iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} icon={WorkIcon}>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work" date="2010 - 2011" iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} icon={WorkIcon}>
                         <h3 className="vertical-timeline-element-title">Art Director</h3>
                         <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
                         <p>Creative Direction, User Experience, Visual Design, SEO, Online Marketing</p>
                     </VerticalTimelineElement>
+
                     <VerticalTimelineElement className="vertical-timeline-element--work" date="2008 - 2010" iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} icon={WorkIcon}>
                         <h3 className="vertical-timeline-element-title">Web Designer</h3>
                         <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
                         <p>User Experience, Visual Design</p>
                     </VerticalTimelineElement>
+
                     <VerticalTimelineElement className="vertical-timeline-element--work" date="2006 - 2008" iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} icon={WorkIcon}>
                         <h3 className="vertical-timeline-element-title">Web Designer</h3>
                         <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
                         <p>User Experience, Visual Design</p>
                     </VerticalTimelineElement>
+
                     <VerticalTimelineElement className="vertical-timeline-element--education" date="April 2013" iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }} icon={SchoolIcon}>
                         <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
                         <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
                         <p>Strategy, Social Media</p>
                     </VerticalTimelineElement>
+
                     <VerticalTimelineElement className="vertical-timeline-element--education" date="November 2012"  iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }} icon={SchoolIcon}>
                         <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
                         <h4 className="vertical-timeline-element-subtitle">Certification</h4>
                         <p>Creative Direction, User Experience, Visual Design</p>
                     </VerticalTimelineElement>
+
                     <VerticalTimelineElement className="vertical-timeline-element--education" date="2002 - 2006" iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }} icon={SchoolIcon}>
                         <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
                         <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
                         <p>Creative Direction, Visual Design</p>
                     </VerticalTimelineElement>
+
                     <VerticalTimelineElement iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }} icon={StarIcon}/>
                 </VerticalTimeline>
             </div>
