e2ba305e4c98ffb8664f9d35cdc1c2d37176f4a1
Matt Heslington <matt.heslington@icloud.com>
2021-01-04 21:58:50 +0800

    Launchable

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from "gatsby"
 import logo from 'src/static/petal-logo.png'
 import text from 'src/static/fast-site.png'
-import { HamburgerArrow } from 'react-animated-burgers'
 
 const Nav = () => {
-
-    const [isOpen, setIsOpen] = useState(false);
-
-    function toggle() {
-        setIsOpen(prevState => !prevState);
-        console.log(isOpen);
-    }
 
     return (
         <div className="z-20 flex flex-row items-center justify-between h-24 -mb-24">
@@ -22,26 +14,26 @@ const Nav = () => {
             </div>
             <div>
                 <ul className="flex flex-row items-center space-x-4 sm:space-x-12">
-                    <li className="text-sm font-light highlight navlink">
-                        <Link to="/working-with-petal" data-content="Working with Petal" className="highlight">Working with Petal</Link>
-                    </li>
+
                     <li className="text-sm font-light">
                         <div className="navlink">
-                            <Link to="/examples" data-content="Services" className="highlight">Services</Link>
+                            <Link to="/working-with-petal" data-content="Working with Petal" className="highlight">Working with Petal</Link>
                         </div>
 
                         <div className="absolute w-40 mt-2 mr-12">
                             <img className="" src={text} alt="see how fast our site is"/>
                         </div>
                     </li>
+
                     <li>
                         <Link to="/schedule">
-                            <button className="flex items-center px-5 py-3 text-sm font-light border border-white rounded-full bg-footerBG">
+                            <button className="flex items-center px-4 py-2 text-sm font-light border border-white rounded-full sm:px-5 sm:py-3 bg-footerBG">
                                 Schedule a Call&nbsp;
                                 <svg className="inline-block w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" /></svg>
                             </button>
                         </Link>
                     </li>
+
                 </ul>
             </div>
         </div>
@@ -49,5 +41,3 @@ const Nav = () => {
 }
 
 export default Nav
-
-//TODO: Put a transition time on everything
