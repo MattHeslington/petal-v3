01ae0b0fed06134fbce3865607aa85fc746c1342
Matt Heslington <matt.heslington@icloud.com>
2021-01-05 14:34:38 +0800

    Change heading size on phone

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ const About = () => {
     return (
         <div className="flex flex-col items-center h-screen mb-64 lg:flex-row sm:mb-48">
             <div className="flex flex-col justify-center w-full h-screen px-12 lg:w-1/2">
-                <h2 className="font-sans font-bold leading-snug tracking-tight text-center text-white sm:text-5xl lg:text-left">You're in <span className="gradienta">great hands</span>.</h2>
+                <h2 className="font-sans text-4xl font-bold leading-snug tracking-tight text-center text-white sm:text-5xl lg:text-left">You're in <span className="gradienta">great hands</span>.</h2>
                 <p className="mt-4 font-sans text-lg font-light leading-loose text-center lg:text-left">Petal is a small boutique design studio dedicated to achieving outstanding results using the latest web technologies. We have over twenty years of experience of web design and development, have also run stunningly successful digital marketing campaigns in some of the toughest market-segments in the world, and have turned small online businesses into multi-million dollar operations.</p>
             </div>
             <div className="flex items-center justify-center w-full px-12 lg:w-1/2">
